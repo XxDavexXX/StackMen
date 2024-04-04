527b80d9008f25371446f8315d8c3cb333ec1754
XxDavexX <dave.santivanez@gmail.com>
2024-04-04 09:35:21 -0500

    avance

--- src/index.js
+++ src/index.js
@@ -4,20 +4,17 @@ import {engine} from 'express-handlebars';
 import {join, dirname} from 'path';
 import { fileURLToPath } from 'url';
 import clientesRoutes from './routes/clientes.routes.js';
+import handlebars from 'handlebars';
 
 //Initialize
-
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 //Settings
-
 app.set('port', process.env.PORT || 3000);
-
 app.set('views', join(__dirname, 'views'));
 
 //Configurar motors de plantilla
-
 app.engine('.hbs', engine({
     defaultLayout: 'main',
     layoutsDir: join(app.get('views'), 'layouts'),
@@ -28,17 +25,19 @@ app.engine('.hbs', engine({
 app.set('view engine', '.hbs');
 
 //Middlewares
-
 app.use(morgan('dev'));
 
 //Express para trabajar con interfaces y formularios 
 //y para trabajar con archivos tipo Json
-
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-//Routes
+// Registrar el helper eq manualmente
+handlebars.registerHelper('eq', function(arg1, arg2, options) {
+    return arg1 === arg2 ? options.fn(this) : options.inverse(this);
+});
 
+//Routes
 app.get('/', (req, res) => {
     res.render('index');
     // res.json({'message': 'Hola mundo'})
@@ -51,7 +50,6 @@ app.use(clientesRoutes)
 app.use(express.static(join(__dirname, 'public')));
 
 //Run Server
-
 app.listen(app.get('port'), () =>
     console.log('cargando listeninng', app.get('port'))
 );
--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -3,15 +3,22 @@ import pool from './../database/databse.js';
 
 const router = Router();
 
-router.get('/add', (req, res) => {
-    res.render('clientes/add');
+router.get('/add', async (req, res) => {
+    try {
+        const [relaciones] = await pool.query('SELECT * FROM relacion');
+        const [tiposCliente] = await pool.query('SELECT * FROM tipocliente');
+        
+        res.render('clientes/add', { relaciones, tiposCliente });
+    } catch(err) {
+        res.status(500).json({ message: err.message });
+    }
 });
 
 router.post('/add', async(req, res) => {
     try {
-        const {nomcli, apecli, nrodnicli, telcli} = req.body;
+        const {nomcli, apecli, nrodnicli, telcli, idrelacion, idtipocliente} = req.body;
         const newCliente = {
-            nomcli, apecli, nrodnicli, telcli
+            nomcli, apecli, nrodnicli, telcli, idrelacion, idtipocliente
         }
         await pool.query('INSERT INTO cliente SET ?', [newCliente]);
         res.redirect('/list');
@@ -23,7 +30,8 @@ router.post('/add', async(req, res) => {
 
 router.get('/list', async(req, res) => {
     try{
-        const [result] = await pool.query('SELECT * FROM cliente');
+        const [result] = await pool.query('SELECT cliente.*, TIPOCLIENTE.tipocliente AS tipo_cliente, RELACION.estado AS relacion FROM cliente INNER JOIN RELACION ON RELACION.id = cliente.idrelacion INNER JOIN TIPOCLIENTE ON TIPOCLIENTE.id = cliente.idtipocliente');
+
         res.render('clientes/list', {clientes: result});
     }catch(err){
         res.status(500).json({message: err.message});
@@ -33,38 +41,37 @@ router.get('/list', async(req, res) => {
 router.get('/edit/:idcli', async(req, res) => {
     try {
         const { idcli } = req.params;
-        console.log("idcli:", idcli);
         const [cliente] = await pool.query('SELECT * FROM cliente WHERE idcli = ?', [idcli]);
-        console.log("query:", cliente);
         if (!cliente || cliente.length === 0) {
-            // Manejar el caso donde no se encuentra ningún cliente con el idcli proporcionado
-            // Por ejemplo, redirigir a una página de error o mostrar un mensaje adecuado
             return res.status(404).json({ message: 'Cliente no encontrado' });
         }
-        const clienteEdit = cliente[0];
-        res.render('clientes/edit', { cliente: clienteEdit });
+        const [relaciones] = await pool.query('SELECT * FROM relacion');
+        const [tiposCliente] = await pool.query('SELECT * FROM tipocliente');
+        res.render('clientes/edit', { cliente: cliente[0], relaciones, tiposCliente });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.post('/edit/:idcli', async (req, res) => {
+    try {
+        const { nomcli, apecli, nrodnicli, telcli, idrelacion, idtipocliente } = req.body;
+        const { idcli } = req.params;
+        const editCliente = { nomcli, apecli, nrodnicli, telcli, idrelacion, idtipocliente };
+        await pool.query('UPDATE cliente SET ? WHERE idcli = ?', [editCliente, idcli]);
+        res.redirect('/list');
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 });
 
 
-router.post('/edit/:idcli', async(req, res) => {
-    try {
-        const {nomcli, apecli, nrodnicli, telcli} = req.body;
-        const {idcli} = req.params;
-        const editCliente = {nomcli, apecli, nrodnicli, telcli};
-        await pool.query('UPDATE cliente SET ? WHERE idcli = ?', [editCliente, idcli]);
-        res.redirect('/list')
-    } catch (err) {
-        res.status(500).json({message: err.message});
-    }
-});
 
 router.get('/delete/:idcli', async(req, res) => {
     try {
         const {idcli} = req.params;
         await pool.query('DELETE FROM cliente WHERE idcli = ?', [idcli]);
+        res.redirect('/list');
     } catch (err) {
         res.status(500).json({message: err.message});
     }
